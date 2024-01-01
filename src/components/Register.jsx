b0ea3fea21b05c7e6452b84b769732378f6c4b96
RamRijal <rijram004@gmail.com>
2024-01-01 22:16:58 +0545

    Register added

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Register = () => {
+    const [username,setUsername]=useState('')
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    let onSubmit=(e)=>{
+        e.preventDefault();
+        console.log("Submitted");
+    }
   return (
-    <div>
-        
+  <>  <div>
+        <div className='register-form'>
+            <form onSubmit={onsubmit}>
+                <h2>Register</h2>
+                <label htmlFor="username">Username: </label>
+                <input type="text" id="username" name="username" value={username}/><br/>
+
+                <label htmlFor="email">Email: </label>
+                <input type="email" id="email" name="email" value={email}/><br/>
+
+                <label htmlFor="password">Password: </label>
+                <input 
+                type="password" 
+                id="password" 
+                name="password"
+                value={password}/>
+                <br/>
+                <button 
+                type="submit"
+                >Register</button>
+
+            </form>
+        </div>
     </div>
+    </>
   )
 }
 
