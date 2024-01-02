36eb823aa0449c42d8ee4b50f2b3408b59632c94
RamRijal <rijram004@gmail.com>
2024-01-02 22:08:43 +0545

    Register added 2.0

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,41 +1,87 @@
 import React, { useState } from 'react'
+import Css from "./css.css"
 
 const Register = () => {
-    const [username,setUsername]=useState('')
+    const [username, setUsername] = useState('')
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
 
-    let onSubmit=(e)=>{
+    let onSubmit = (e) => {
         e.preventDefault();
-        console.log("Submitted");
+        console.log("Submitted Successfully");
+
+        const data = {
+            username: username,
+            email: email,
+            password: password,
+            confirmPassword: confirmPassword
+        }
+        console.log(data);
+
+        
+
     }
-  return (
-  <>  <div>
-        <div className='register-form'>
-            <form onSubmit={onsubmit}>
-                <h2>Register</h2>
-                <label htmlFor="username">Username: </label>
-                <input type="text" id="username" name="username" value={username}/><br/>
+    return (
+        <>  <div>
+            <div className='register-form'>
+                <form onSubmit={onSubmit}>
+                    <h2>Register</h2>
+                    <label htmlFor="username">Username: </label>
+                    <br />
 
-                <label htmlFor="email">Email: </label>
-                <input type="email" id="email" name="email" value={email}/><br/>
+                    <input
+                        type="text"
+                        id="username"
+                        name="username"
+                        value={username}
+                        onChange={(e) => {
+                            setUsername(e.target.value)
+                        }} />
+                    <br />
 
-                <label htmlFor="password">Password: </label>
-                <input 
-                type="password" 
-                id="password" 
-                name="password"
-                value={password}/>
-                <br/>
-                <button 
-                type="submit"
-                >Register</button>
+                    <label htmlFor="email">Email: </label>
+                    <br />
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        value={email}
+                        onChange={(e) => {
+                            setEmail(e.target.value);
+                        }} /><br />
 
-            </form>
+                    <label htmlFor="password">Password: </label>
+                    <br />
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        value={password}
+                        onChange={(e) => {
+                            setPassword(e.target.value)
+                        }} />
+                    <br />
+                    <label htmlFor="confirmpassword">Confirm Password: </label>
+                    <br />
+                    <input
+                        type="password"
+                        id="confirmpassword"
+                        name="confirmpassword"
+                        value={confirmPassword}
+                        onChange={(e) => {
+                            setConfirmPassword(e.target.value)
+                        }} />
+                    <br />
+                    <button
+                        type="submit"
+                    >Register</button>
+
+                </form>
+            </div>
         </div>
-    </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Register
